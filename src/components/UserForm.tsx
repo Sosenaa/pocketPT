b1186327fee2b30109c55e7f907766da192cfbf6
Krystian Sosnowski <k.sosnowski1695@gmail.com>
2026-04-20 19:02:49 +0100

    Fix bug - Vercel. UseState removed

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_BASE_URL } from "../api";
 import "../App.css";
